edb848853b2eb48ca9e3a34691479427c7da7389
kpedrok <pedrokvb@gmail.com>
2021-04-25 10:20:04 -0300

    Update plant select page

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,11 +1,31 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { EnvironmentButton } from "../components/EnvironmentButton";
+import { Header } from "../components/Header";
 import colors from "../styles/colors";
+import fonts from "../styles/fonts";
 
 export function PlantSelect() {
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <View style={styles.header}>
+        <Header />
+
+        <Text style={styles.title}>Em qual ambiente</Text>
+        <Text style={styles.subtitle}>você quer colocar a sua planta?</Text>
+      </View>
+
+      <View>
+        <FlatList
+          data={[1, 2, 3, 4, 5]}
+          renderItem={({ item }) => (
+            <EnvironmentButton title="Cozinha" active />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.environmentList}
+        />
+      </View>
     </View>
   );
 }
@@ -14,7 +34,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.background,
-    alignItems: "center",
+  },
+  header: {
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15,
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    fontSize: 17,
+    lineHeight: 20,
+    color: colors.heading,
+  },
+  environmentList: {
+    height: 40,
     justifyContent: "center",
+    paddingBottom: 5,
+    marginLeft: 32,
+    marginVertical: 32,
   },
 });
