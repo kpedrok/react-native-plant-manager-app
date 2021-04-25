fbb00f77943bab285351a402217289bd641a5ff3
kpedrok <pedrokvb@gmail.com>
2021-04-25 10:00:09 -0300

    Add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import userImg from "../assets/pedro.png";
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.userName}>Rodrigo</Text>
+      </View>
+
+      <Image source={userImg} style={styles.image}></Image>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 20,
+    marginTop: getStatusBarHeight(),
+    padding: 20,
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  userName: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 40,
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 40,
+  },
+});
