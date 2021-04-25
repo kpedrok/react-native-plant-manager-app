93e9bb2acabb71e9b3dc2c9a57eaecb657b5c543
kpedrok <pedrokvb@gmail.com>
2021-04-25 00:24:38 -0300

    Add confirmation page

---
+++ src/pages/Confirmation.tsx
@@ -0,0 +1,87 @@
+import { useNavigation, useRoute } from "@react-navigation/native";
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Button } from "../components/Button";
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+
+interface Params {
+  title: string;
+  subtitle: string;
+  buttonTitle: string;
+  icon: "smile" | "hug";
+  nextScreen: string;
+}
+
+const emojis = {
+  hug: "🤗",
+  smile: "😄",
+};
+
+export function Confirmation() {
+  const navigation = useNavigation();
+  const routes = useRoute();
+
+  // const {
+  //   title,
+  //   subtitle,
+  //   buttonTitle,
+  //   icon,
+  //   nextScreen,
+  // } = routes.params as Params;
+
+  function handleNextScreen() {
+    // navigation.navigate(nextScreen);
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.emoji}>emoji</Text>
+        <Text style={styles.title}>title</Text>
+        <Text style={styles.subtitle}>subtitle</Text>
+        <View style={styles.footer}>
+          <Button title={"buttonTitle"} onPress={handleNextScreen} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    padding: 30,
+  },
+  emoji: {
+    fontSize: 78,
+  },
+  title: {
+    fontSize: 22,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    textAlign: "center",
+    lineHeight: 38,
+    marginTop: 15,
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    textAlign: "center",
+    fontSize: 17,
+    paddingVertical: 10,
+    color: colors.heading,
+  },
+  footer: {
+    width: "100%",
+    paddingHorizontal: 50,
+    marginTop: 20,
+  },
+});
