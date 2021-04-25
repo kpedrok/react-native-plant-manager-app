63081102dc3536c3a179af9f9c6cf3b74beba1d6
kpedrok <pedrokvb@gmail.com>
2021-04-25 15:36:14 -0300

    Add json server

--- README.md
+++ README.md
@@ -5,3 +5,17 @@
 ### Run
 - yarn install
 - expo start
+
+
+
+https://www.youtube.com/watch?v=HZfuL7pN9Wo& 
+parei em 44s
+
+ipconfig getifaddr en0 
+
+
+ npm i -g json-server
+
+
+## Start server
+ json-server ./src/services/server.json --host 192.168.2.87 --port 3333
--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import { EnvironmentButton } from "../components/EnvironmentButton";
 import { Header } from "../components/Header";
+import api from "../services/api";
 import colors from "../styles/colors";
 import fonts from "../styles/fonts";
 
+interface EnvironmentProps {
+  key: string;
+  title: string;
+}
+
 export function PlantSelect() {
+  const [environments, setEnvironment] = useState<EnvironmentProps[]>([]);
+
+  useEffect(() => {
+    async function fetchEnvironment() {
+      const { data } = await api.get("plants_environments");
+      setEnvironment(data);
+    }
+    fetchEnvironment();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -17,10 +33,8 @@ export function PlantSelect() {
 
       <View>
         <FlatList
-          data={[1, 2, 3, 4, 5]}
-          renderItem={({ item }) => (
-            <EnvironmentButton title="Cozinha" active />
-          )}
+          data={environments}
+          renderItem={({ item }) => <EnvironmentButton title={item.title} />}
           horizontal
           showsHorizontalScrollIndicator={false}
           contentContainerStyle={styles.environmentList}
