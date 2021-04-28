48f14533015a4a0f89e6752112b5718b5241cbae
kpedrok <pedrokvb@gmail.com>
2021-04-28 01:25:27 -0300

    Add tabs

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,12 +1,11 @@
 import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react'
 import { Confirmation } from '../pages/Confirmation'
-import { MyPlants } from '../pages/MyPlants'
 import PlantSave from '../pages/PlantSave'
-import { PlantSelect } from '../pages/PlantSelect'
 import { UserIdentification } from '../pages/UserIdentification'
 import { Welcome } from '../pages/Welcome'
 import colors from '../styles/colors'
+import AuthRoutes from './tab.routes'
 
 const StackRoutes = createStackNavigator()
 
@@ -25,9 +24,9 @@ const AppRoutes: React.FC = () => (
       component={UserIdentification}
     />
     <StackRoutes.Screen name='Confirmation' component={Confirmation} />
-    <StackRoutes.Screen name='PlantSelect' component={PlantSelect} />
+    <StackRoutes.Screen name='PlantSelect' component={AuthRoutes} />
     <StackRoutes.Screen name='PlantSave' component={PlantSave} />
-    <StackRoutes.Screen name='MyPlants' component={MyPlants} />
+    <StackRoutes.Screen name='MyPlants' component={AuthRoutes} />
   </StackRoutes.Navigator>
 )
 
