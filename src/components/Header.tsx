b847a5ae422e0438ad8a36323ff14f237b5c310a
kpedrok <pedrokvb@gmail.com>
2021-04-27 23:53:36 -0300

    Save username on storage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,46 +1,59 @@
-import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
-import { getStatusBarHeight } from "react-native-iphone-x-helper";
-import userImg from "../assets/pedro.png";
-import colors from "../styles/colors";
-import fonts from "../styles/fonts";
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import React, { useEffect, useState } from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+import userImg from '../assets/pedro.png'
+import colors from '../styles/colors'
+import fonts from '../styles/fonts'
 
 export function Header() {
+  const [userName, setUserName] = useState<string>()
+
+  useEffect(() => {
+    async function loadStorageUserName() {
+      const user = await AsyncStorage.getItem('@plantmanager:user')
+
+      setUserName(user || '')
+    }
+
+    loadStorageUserName()
+  }, [userName])
+
   return (
     <View style={styles.container}>
       <View>
         <Text style={styles.greeting}>Olá,</Text>
-        <Text style={styles.userName}>Pedro</Text>
+        <Text style={styles.username}>{userName}</Text>
       </View>
 
-      <Image source={userImg} style={styles.image}></Image>
+      <Image source={userImg} style={styles.image} />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     paddingVertical: 20,
     marginTop: getStatusBarHeight(),
-  },
-  greeting: {
-    fontSize: 32,
-    color: colors.heading,
-    fontFamily: fonts.text,
-  },
-  userName: {
-    fontSize: 32,
-    color: colors.heading,
-    fontFamily: fonts.heading,
-    lineHeight: 40,
   },
   image: {
     width: 70,
     height: 70,
     borderRadius: 40,
   },
-});
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  username: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40,
+  },
+})
--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -1,5 +1,6 @@
-import { useNavigation } from "@react-navigation/native";
-import React, { useState } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useNavigation } from '@react-navigation/native'
+import React, { useState } from 'react'
 import {
   Alert,
   Keyboard,
@@ -11,63 +12,63 @@ import {
   TextInput,
   TouchableWithoutFeedback,
   View,
-} from "react-native";
-import { Button } from "../components/Button";
-import colors from "../styles/colors";
-import fonts from "../styles/fonts";
+} from 'react-native'
+import { Button } from '../components/Button'
+import colors from '../styles/colors'
+import fonts from '../styles/fonts'
 
 export function UserIdentification() {
-  const navigation = useNavigation();
+  const navigation = useNavigation()
 
-  const [isFocused, setIsFocused] = useState(false);
-  const [isFilled, setIsFilled] = useState(false);
-  const [name, setName] = useState<string>();
+  const [isFocused, setIsFocused] = useState(false)
+  const [isFilled, setIsFilled] = useState(false)
+  const [name, setName] = useState<string>()
 
   function handleInputBlur() {
-    setIsFocused(false);
-    setIsFilled(!!name);
+    setIsFocused(false)
+    setIsFilled(!!name)
   }
 
   function handleInputFocus() {
-    setIsFocused(true);
+    setIsFocused(true)
   }
 
   function handleInputChange(value: string) {
-    setName(value);
-    setIsFilled(!!value);
+    setName(value)
+    setIsFilled(!!value)
   }
 
   async function handleSubmit() {
-    if (!name) return Alert.alert("Me diz como chamar você 😢");
-    navigation.navigate("Confirmation");
+    if (!name) return Alert.alert('Me diz como chamar você 😢')
+    navigation.navigate('Confirmation')
 
-    // try {
-    //   await AsyncStorage.setItem("@plantmanager:user", name);
-    //   navigation.navigate("Confirmation", {
-    //     title: "Prontinho",
-    //     subtitle: `Agora vamos começar a cuidar das suas plantinhas com muito cuidado.`,
-    //     buttonTitle: "Começar",
-    //     icon: "smile",
-    //     nextScreen: "PlantSelect",
-    //   });
-    // } catch {
-    //   Alert.alert("Não foi possível salvar o seu nome. 😢");
-    // }
+    try {
+      await AsyncStorage.setItem('@plantmanager:user', name) //Add @ and app name to ensure that other app wont overwrite
+      navigation.navigate('Confirmation', {
+        title: 'Prontinho',
+        subtitle: `Agora vamos começar a cuidar das suas plantinhas com muito cuidado.`,
+        buttonTitle: 'Começar',
+        icon: 'smile',
+        nextScreen: 'PlantSelect',
+      })
+    } catch {
+      Alert.alert('Não foi possível salvar o seu nome. 😢')
+    }
   }
 
   return (
     <SafeAreaView style={styles.container}>
       <KeyboardAvoidingView
         style={styles.container}
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       >
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
           <View style={styles.content}>
             <View style={styles.form}>
               <View style={styles.header}>
-                <Text style={styles.emoji}>{isFilled ? "😄" : "😃"}</Text>
+                <Text style={styles.emoji}>{isFilled ? '😄' : '😃'}</Text>
                 <Text style={styles.title}>
-                  Como podemos {"\n"}
+                  Como podemos {'\n'}
                   chamar você?
                 </Text>
               </View>
@@ -77,42 +78,42 @@ export function UserIdentification() {
                   styles.input,
                   (isFocused || isFilled) && { borderColor: colors.green },
                 ]}
-                placeholder="Digite um nome"
+                placeholder='Digite um nome'
                 onBlur={handleInputBlur}
                 onFocus={handleInputFocus}
                 onChangeText={handleInputChange}
               />
 
               <View style={styles.footer}>
-                <Button title="Confirmar" onPress={handleSubmit} />
+                <Button title='Confirmar' onPress={handleSubmit} />
               </View>
             </View>
           </View>
         </TouchableWithoutFeedback>
       </KeyboardAvoidingView>
     </SafeAreaView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: "100%",
-    justifyContent: "space-around",
-    alignItems: "center",
+    width: '100%',
+    justifyContent: 'space-around',
+    alignItems: 'center',
   },
   content: {
     flex: 1,
-    width: "100%",
+    width: '100%',
   },
   form: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: 'center',
     paddingHorizontal: 54,
-    alignItems: "center",
+    alignItems: 'center',
   },
   header: {
-    alignItems: "center",
+    alignItems: 'center',
   },
   emoji: {
     fontSize: 44,
@@ -121,23 +122,23 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderColor: colors.gray,
     color: colors.heading,
-    width: "100%",
+    width: '100%',
     fontSize: 18,
     marginTop: 50,
     padding: 10,
-    textAlign: "center",
+    textAlign: 'center',
   },
   title: {
     fontSize: 24,
     lineHeight: 32,
-    textAlign: "center",
+    textAlign: 'center',
     color: colors.heading,
     fontFamily: fonts.heading,
     marginTop: 20,
   },
   footer: {
-    width: "100%",
+    width: '100%',
     marginTop: 40,
     paddingHorizontal: 20,
   },
-});
+})
